0726cf0b0c3a42b8ed6d60e97d6d7e6672157580
Maxwell Bernstein <tekknolagi@gmail.com>
2017-11-12 01:09:20 -0500

    Remove Ruby scripts

--- transform_csv.rb
+++
@@ -1,7 +0,0 @@
-require 'csv'
-
-(CSV.readlines 'current.csv').each do |a|
-    name, url, section = a
-# linkgroup(:name => 'TuftsTools', :website => 'https://tuftstools.tufts.edu/', :category_id => 'Student Services')
-    puts "linkgroup(:name => '#{name}', :website => '#{url}', :category_id => '#{section}')"
-end
